ac8cee8f02f5a00a28e88a94a35ac1507e601475
mrshishkebabs <xkenxparkx@gmail.com>
2022-12-15 10:36:15 -0500

    Error Fix

--- Assets/FinalsAssets/Scripts/FollowMe.cs
+++ Assets/FinalsAssets/Scripts/FollowMe.cs
@@ -7,12 +7,14 @@ public class FollowMe : MonoBehaviour
     public GameObject player;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, player.transform.position.y+2, transform.position.z);
-
+        if (player != null)
+        { 
+            transform.position = new Vector3(transform.position.x, player.transform.position.y + 2, transform.position.z);
+        }
     }
 }
